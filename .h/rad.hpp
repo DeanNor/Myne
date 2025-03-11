96c5d10cbb7045669e6d97766abcbbd2a8a505d7
DeanNor <152406459+DeanNor@users.noreply.github.com>
2025-03-10 21:44:29 -0500

    Update rad.hpp

--- .h/rad.hpp
+++ .h/rad.hpp
@@ -28,168 +28,42 @@ public:
     // ----- +
     rad operator+ (rad amount);
 
-    long double operator+ (long double amount);
-
-    double operator+ (double amount);
-
-    float operator+ (float amount);
-
-    unsigned long long operator+ (unsigned long long amount);
-
-    unsigned long int operator+ (unsigned long int amount);
-
-    long long operator+ (long long amount);
-
-    long operator+ (long amount);
-
-    int operator+ (int amount);
-
-    short operator+ (short amount);
+    rad operator+ (double amount);
 
     // ----- +=
     rad& operator+= (rad amount);
 
-    rad& operator+= (long double amount);
-
     rad& operator+= (double amount);
-
-    rad& operator+= (float amount);
-
-    rad& operator+= (unsigned long long amount);
-
-    rad& operator+= (unsigned long int amount);
-
-    rad& operator+= (long long amount);
-
-    rad& operator+= (long amount);
-
-    rad& operator+= (int amount);
-
-    rad& operator+= (short amount);
 
     // ----- -
     rad operator- (rad amount);
 
-    long double operator- (long double amount);
-
-    double operator- (double amount);
-
-    float operator- (float amount);
-
-    unsigned long long operator- (unsigned long long amount);
-
-    unsigned long int operator- (unsigned long int amount);
-
-    long long operator- (long long amount);
-
-    long operator- (long amount);
-
-    int operator- (int amount);
-
-    short operator- (short amount);
+    rad operator- (double amount);
 
     // ----- -=
     rad& operator-= (rad amount);
 
-    rad& operator-= (long double amount);
-
     rad& operator-= (double amount);
-
-    rad& operator-= (float amount);
-
-    rad& operator-= (unsigned long long amount);
-
-    rad& operator-= (unsigned long int amount);
-
-    rad& operator-= (long long amount);
-
-    rad& operator-= (long amount);
-
-    rad& operator-= (int amount);
-
-    rad& operator-= (short amount);
 
     // ----- *
     rad operator* (rad amount);
 
-    long double operator* (long double amount);
-
-    double operator* (double amount);
-
-    float operator* (float amount);
-
-    unsigned long long operator* (unsigned long long amount);
-
-    unsigned long int operator* (unsigned long int amount);
-
-    long long operator* (long long amount);
-
-    long operator* (long amount);
-
-    int operator* (int amount);
-
-    short operator* (short amount);
-
+    rad operator* (double amount);
+    
     // ----- *=
     rad& operator*= (rad amount);
-
-    rad& operator*= (long double amount);
-
+    
     rad& operator*= (double amount);
-
-    rad& operator*= (float amount);
-
-    rad& operator*= (unsigned long long amount);
-
-    rad& operator*= (unsigned long int amount);
-
-    rad& operator*= (long long amount);
-
-    rad& operator*= (long amount);
-
-    rad& operator*= (int amount);
-
-    rad& operator*= (short amount);
 
     // ----- /
     rad operator/ (rad amount);
 
-    long double operator/ (long double amount);
-
     double operator/ (double amount);
-
-    float operator/ (float amount);
-
-    unsigned long long operator/ (unsigned long long amount);
-
-    unsigned long int operator/ (unsigned long int amount);
-
-    long long operator/ (long long amount);
-
-    long operator/ (long amount);
-
-    int operator/ (int amount);
-
-    short operator/ (short amount);
 
     // ----- /=
     rad& operator/= (rad amount);
 
-    rad& operator/= (long double amount);
-
     rad& operator/= (double amount);
-
-    rad& operator/= (float amount);
-
-    rad& operator/= (unsigned long long amount);
-
-    rad& operator/= (unsigned long int amount);
-
-    rad& operator/= (long long amount);
-
-    rad& operator/= (long amount);
-
-    rad& operator/= (int amount);
-
-    rad& operator/= (short amount);
 };
+
+rad operator rad(double amount);
